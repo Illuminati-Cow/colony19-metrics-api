e4c8d9bc673e9f9ffa0863075399eb485b1df580
Cole Falxa-Sturken <colefalxa@gmail.com>
2025-05-15 15:32:49 -0700

    Ignore .env files and allow port to be configured
    Add .dockerignore and update .gitignore to exclude __pycache__
    
    Delete app/__pycache__ directory

--- app/main.py
+++ app/main.py
@@ -1,25 +1,62 @@
-from fastapi import FastAPI, Request
-import sqlite3
+from datetime import datetime
+from unittest import result
+from fastapi import FastAPI
+from flask import session
+from pydantic import BaseModel
+from pymongo import MongoClient
 import os
 
-DB_PATH = "/data/metrics.db"
+DB_URI = "mongodb://mongodb:27017"
+DB_NAME = "metrics"
 
 app = FastAPI()
 
-# Ensure DB exists
-os.makedirs("/data", exist_ok=True)
-conn = sqlite3.connect(DB_PATH)
-conn.execute("CREATE TABLE IF NOT EXISTS metrics (payload TEXT)")
-conn.close()
+# Initialize MongoDB client
+client = MongoClient(DB_URI)
+db = client[DB_NAME]
 
-@app.post("/metrics")
-async def collect_metrics(request: Request):
-    data = await request.json()
-    conn = sqlite3.connect(DB_PATH)
-    conn.execute("INSERT INTO metrics (payload) VALUES (?)", (str(data),))
-    conn.commit()
-    conn.close()
-    return {"status": "ok"}
+# Ensure collections exist
+db.sessions.create_index("session_id", unique=True)
+
+class SessionData(BaseModel):
+    session_id: str
+    app_name: str
+    device: str
+    os: str
+    fps: list[int]
+
+
+    
+
+@app.post("/metrics/new_session")
+def create_session(app_name: str):
+    session_id = str(datetime.now())
+    db.sessions.insert_one({
+        "session_id": session_id,
+        "app_name": app_name,
+    })
+    return {"status": "ok", "session_id": session_id}
+
+# @app.put("/metrics/{session_id}")
+# def update_data(session: SessionData):
+#     db.sessions.update_one(
+#         {"session_id": session.session_id},
+#         {"$set": {
+            
+#         upsert=True
+#     )
+#     return {"status": "ok"}
+
+
+
+@app.get("/metrics")
+async def get_metrics():
+    sessions = list(db.sessions.find({}, {"_id": 0}))
+    return {"metrics": sessions}
+
+@app.get("/")
+async def root():
+    return {"message": "Welcome to the Metrics Collector API!"}
 
 @app.get("/health")
 async def healthcheck():
